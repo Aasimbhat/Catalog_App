f64e0dfd22c49a84f2e90ffd8e2b5edfd06d7b3d
Kamran-7 <90784719+Aasimbhat@users.noreply.github.com>
2022-11-27 14:48:28 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_30days/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) { //build is a imp method where all ui related work happens
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to flutter"),
-          ),
-        ),
-      ),
+      home:HomePage()
     );
   }
 }
