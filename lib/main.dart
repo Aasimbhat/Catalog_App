d54937ab62311ef0484230d6161376b48792b06a
Kamran-7 <90784719+Aasimbhat@users.noreply.github.com>
2022-11-28 16:37:50 +0530

    drawer changes

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness:Brightness.dark
       ),
-      initialRoute: "/login",
+      initialRoute:MyRoutes.homeRoute ,
       routes: {
         MyRoutes.loginRoute :(context)=>LoginPage(),
         MyRoutes.homeRoute :(context)=>HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_30days/widgets/drawer.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -14,7 +15,9 @@ class HomePage extends StatelessWidget {
             child: Text("Welcome to flutter"),
           ),
         ),
-        drawer: Drawer(),
+        drawer: MyDrawer(
+          
+        ),
       );
   }
 }
