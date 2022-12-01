9e9276122e88074d9a87942a57d7217b0146fe36
Kamran-7 <90784719+Aasimbhat@users.noreply.github.com>
2022-12-01 16:08:41 +0530

    grid view gridtile day 15 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,56 +1,92 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_30days/models/catalog.dart';
 import 'package:flutter_30days/widgets/drawer.dart';
 import 'package:flutter_30days/widgets/item_widget.dart';
-class HomePage extends StatefulWidget {
 
+class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     super.initState();
     loadData();
   }
-  loadData()async{
-   var catalogJson= await rootBundle.loadString(
-      "assets/files/catalog.json"
-    );
-    var decodeData= jsonDecode(catalogJson);
-    var productsData=decodeData["products"];
-    CatalogModel.items=List.from(productsData)
-    .map<Item>((item) =>Item.fromMap(item) ).toList();
-    setState(() {
-      
-    });
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-      
-        title:  Text("Catalog App",
-      ),
-      ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child:(CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
-            itemCount:  CatalogModel.items.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(item: CatalogModel.items[index]);
-            },
-          )
-          :Center(
-            child: CircularProgressIndicator(),
-          ),
+        title: Text(
+          "Catalog App",
         ),
-        drawer: MyDrawer(),
-      );
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16
+                    
+                    
+                    
+                    ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18)),
+                      child: GridTile(
+                        header: Container(
+                          padding: EdgeInsets.all(16),
+                          
+                          child: Text(item.name,
+                          style:TextStyle(
+                            color: Colors.white,
+                          ) ,),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple
+                        ),
+                        ),
+                        child: Image.network(item.image),
+                        footer:Container(
+                          padding: EdgeInsets.all(16),
+                          
+                          child: Text(item.price.toString(),
+                          style:TextStyle(
+                            color: Colors.white,
+                          ) ,),
+                        decoration: BoxDecoration(
+                          color: Colors.black
+                        ),
+                        ),
+                      ));
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+      drawer: MyDrawer(),
+    );
   }
 }
